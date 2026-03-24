02be65ce2d61aad34a0ef4993cafd7447021f3dd
rkaur615 <rkaur615@gmail.com>
2026-03-23 17:42:51 -0700

    order class

--- 03-18-2026/cafe.py
+++ 03-18-2026/cafe.py
@@ -6,3 +6,23 @@ class Cofee:
 
     def __str__(self):
         return f"{self.name} - {self.description} {self.base_price}".title()
+    
+
+class Order:
+    def __init__(self, cofee: Cof, size):
+        self.cofee = cofee
+        self.size = size
+        self.price = self.calculate_price()
+
+    def calculate_price(self):
+        final_price = self.coffee.base_price
+
+        if self.size == 'medium':
+            final_price += 0.50
+        elif self.size == 'large':
+            final_price += 1
+        
+        return final_price
+    
+    def __str__(self):
+        return f"{self.size} {self.coffee.name} ${self.price:.2f}".title()
